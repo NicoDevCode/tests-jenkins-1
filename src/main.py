841eb8616ff1df420d127d34a57cbafaf62b2b63
Nicolas Escorcia <nicodevcode@gmail.com>
2019-07-18 17:33:47 -0500

    exe yaa

--- src/main.py
+++ src/main.py
@@ -2,14 +2,10 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-def suma(a, b):
-    return a + b
-
 @app.route('/')
-def hello():
-    res = suma(2,3)	
+def hello():	
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!, {escape(res)}'
+    return f'Hello, {escape(name)}!'
 
 
 if __name__ == "__main__":
